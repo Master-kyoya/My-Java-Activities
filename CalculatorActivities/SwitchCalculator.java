f17d6460cbf3a0889e4633fa1e078ef04d344297
Mikey <71214259+Master-kyoya@users.noreply.github.com>
2021-02-23 10:06:55 +0800

    Create SwitchCalculator.java

---
+++ CalculatorActivities/SwitchCalculator.java
@@ -0,0 +1,33 @@
+package activity4;
+
+import java.util.Scanner;
+
+public class activity4 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Michael Jan R. Tangalin");
+		System.out.println("1-BSIT-1");
+		System.out.print("Enter Num1: ");
+		int num1 = input.nextInt();
+		System.out.print("Enter Num2: ");
+		int num2 = input.nextInt();
+
+		System.out.println("---SWITCH Calculator---");
+		System.out.println("1. Addition");
+		System.out.println("2. Subtraction");
+		System.out.println("3. Multiplication");
+		System.out.println("4. Division");
+
+		System.out.print("Choice: ");
+		int op = input.nextInt();
+
+		switch (op) {
+		case 1:System.out.println(num1 + " + " + num2 + " = " + (num1 + num2)); break;
+		case 2: System.out.println(num1 + " - " + num2 + " = " + (num1 - num2)); break;
+		case 3: System.out.println(num1 + " * " + num2 + " = " + (num1 * num2)); break;
+		case 4: System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));break;
+		default:
+			System.out.println("Invalid Choice");
+		}
+	}
+}
