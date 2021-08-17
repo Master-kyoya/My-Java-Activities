901b7e89e4f2fb39c24faa279f98a447e568d25c
Mikey <71214259+Master-kyoya@users.noreply.github.com>
2021-08-17 20:05:20 +0800

    Create SampleFrame.java

---
+++ AnythingbutNice.java/GUI FILES/SampleFrame.java
@@ -0,0 +1,14 @@
+package gui;
+
+import java.awt.*;
+
+public class SampleFrame extends Frame {
+
+	public static void main(String[] args) {
+		SampleFrame sf = new SampleFrame();
+
+		sf.setSize(100, 100); // Try removing this line
+		sf.setVisible(true); // Try removing this line
+
+	}
+}
